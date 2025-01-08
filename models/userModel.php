10e572768bec3111c1c12c5b2c3e2a8b659e594c
Arthur <arthur.garnier1090@gmail.com>
2025-01-08 13:21:42 +0100

    add user model

--- models/userModel.php
+++ models/userModel.php
@@ -3,55 +3,28 @@
 class User
 {
     private $id;
-    private $name;
+    private $firstname;
+    private $lastname;
+    private $pseudo;
+    private $birth;
     private $email;
+    private $country;
+    private $phone;
     private $password;
+    private $picture;
 
-    public function __construct($id, $name, $email, $password)
+
+    public function __construct($id, $firstname, $lastname, $pseudo, $birth, $email, $country, $phone, $password, $picture)
     {
         $this->id = $id;
-        $this->name = $name;
+        $this->firstname = $firstname;
+        $this->lastname = $lastname;
+        $this->pseudo = $pseudo;
+        $this->birth = $birth;
         $this->email = $email;
+        $this->country = $country;
+        $this->phone = $phone;
         $this->password = $password;
-    }
-
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    public function getName()
-    {
-        return $this->name;
-    }
-
-    public function getEmail()
-    {
-        return $this->email;
-    }
-
-    public function getPassword()
-    {
-        return $this->password;
-    }
-
-    public function setId($id)
-    {
-        $this->id = $id;
-    }
-
-    public function setName($name)
-    {
-        $this->name = $name;
-    }
-
-    public function setEmail($email)
-    {
-        $this->email = $email;
-    }
-
-    public function setPassword($password)
-    {
-        $this->password = $password;
+        $this->picture = $picture;
     }
 }
