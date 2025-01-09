fa69abc3ee30cd8827301199b7940b1ddce63d7b
Arthur <arthur.garnier1090@gmail.com>
2025-01-09 13:34:40 +0100

    fix views

--- views/collaborate.php
+++ views/collaborate.php
@@ -10,18 +10,18 @@
 
     <form action="">
         <label for="">
-        <input type="text" name="recherche" placeholder="Rechercher un artiste, une collab...">
-        <button type="submit" name="recherche"><img src="assets/img/search.svg" alt="loupe"></button>
+            <input type="text" name="recherche" placeholder="Rechercher un artiste, une collab...">
+            <button type="submit" name="recherche"><img src="assets/img/search.svg" alt="loupe"></button>
         </label>
         <span>Filtrer par <img src="assets/img/chevron-up.svg" alt="up"></span>
-        <div>
+        <div id="filter">
             <div>
-            <input type="radio" name="type" id="collab" value="1">
-            <label for="collab">Collaboration</label>
+                <input type="radio" name="type" id="collab" value="1">
+                <label for="collab">Collaboration</label>
             </div>
             <div>
-            <input type="radio" name="type" id="artiste" value="2">
-            <label for="artiste">Artiste</label>
+                <input type="radio" name="type" id="artiste" value="2">
+                <label for="artiste">Artiste</label>
             </div>
             <hr>
             <div>
@@ -32,9 +32,9 @@
                 <input type="radio" name="choix" id="populaire" value="4">
                 <label for="populaire">Plus Populaire</label>
             </div>
-            <label for="recherchetag" id ="tag">
-            <input type="text" name="recherchetag" placeholder="Rechercher un #tag...">
-            <button name="recherchetag"><img src="assets/img/search.svg" alt="loupe"></button></label>
+            <label for="recherchetag" id="tag">
+                <input type="text" name="recherchetag" placeholder="Rechercher un #tag...">
+                <button name="recherchetag"><img src="assets/img/search.svg" alt="loupe"></button></label>
         </div>
     </form>
 
@@ -130,8 +130,8 @@
             </article>
         </section>
         <button id="bRightFiche"><img src="assets/img/circle-chevron-right.svg" alt="fleche droite"></button>
-        
-        </section>
+
+    </section>
 
 
     </section>
