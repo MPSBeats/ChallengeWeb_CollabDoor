0737588c613a48e42f162cf483a1c07da4c09f29
Sacha <sachasimon92350@gmail.com>
2025-01-09 08:55:31 +0100

    fix messaging

--- models/database.php
+++ models/database.php
@@ -5,19 +5,30 @@ class Database
     private $port = "5432";
     private $db_name = "collabdoor";
     private $username = "postgres";
-    private $password = "Ch0c0l@t!n3";
+    private $password = "Mapaaskla1";
     private $conn;
 
     public function connect()
     {
-        $this->conn = null;
-
-        try {
-            $this->conn = new PDO('pgsql:host=' . $this->host . ';port=' . $this->port . ';dbname=' . $this->db_name, $this->username, $this->password);
-        } catch (ErrorException $e) {
-            die('Erreur : ' . $e->getMessage());
+        if ($this->conn === null) {
+            try {
+                $this->conn = new PDO(
+                    'pgsql:host=' . $this->host . ';port=' . $this->port . ';dbname=' . $this->db_name,
+                    $this->username,
+                    $this->password
+                );
+                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $e) {
+                die('Erreur : ' . $e->getMessage());
+            }
         }
-
         return $this->conn;
     }
+
+    public function query($sql)
+    {
+        $this->connect(); // S'assure que la connexion est établie
+        return $this->conn->query($sql);
+    }
 }
+
