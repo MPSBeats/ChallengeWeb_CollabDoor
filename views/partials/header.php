0d3c3c2d16dc54d2f35775845fc16fd9c0f6cba9
Sacha <sachasimon92350@gmail.com>
2025-01-09 11:06:52 +0100

    fix header

--- views/partials/header.php
+++ views/partials/header.php
@@ -9,7 +9,6 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <link rel="stylesheet" href="/assets/css/main.css">
     <script src="https://unpkg.com/lenis@1.1.18/dist/lenis.min.js"></script>
     <title>Document</title>
