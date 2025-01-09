b38bdb6fd6f0ccd651ba4c886cdf3de6dc04d4f9
Arthur <arthur.garnier1090@gmail.com>
2025-01-09 15:04:47 +0100

    update collabdoor.sql

--- collabdoor.sql
+++ collabdoor.sql
@@ -1,159 +1,134 @@
-------------------------------------------------------------
---        Script Postgre 
-------------------------------------------------------------
+DROP TABLE IF EXISTS Users CASCADE;
+DROP TABLE IF EXISTS Collaborations CASCADE;
+DROP TABLE IF EXISTS Media CASCADE;
+DROP TABLE IF EXISTS MediaType CASCADE;
+DROP TABLE IF EXISTS Learnings CASCADE;
+DROP TABLE IF EXISTS Chats CASCADE;
+DROP TABLE IF EXISTS Filters CASCADE;
+DROP TABLE IF EXISTS UserFilters CASCADE;
+DROP TABLE IF EXISTS SearchCollaborationFilters CASCADE;
+DROP TABLE IF EXISTS SearchCollaborations CASCADE;
+DROP TABLE IF EXISTS UserCollaborations CASCADE;
+DROP TABLE IF EXISTS UserLearnings CASCADE;
+DROP TABLE IF EXISTS LearningFilters CASCADE;
 
+CREATE TABLE Users (
+    id_user SERIAL PRIMARY KEY,
+    firstname VARCHAR(50),
+    lastname VARCHAR(50),
+    birth DATE,
+    mail VARCHAR(255) UNIQUE,
+    country VARCHAR(50),
+    phone VARCHAR(15),
+    password VARCHAR(255),
+    picture VARCHAR(255),
+    pseudo VARCHAR(50) UNIQUE
+);
 
+CREATE TABLE Collaborations (
+    id_collaborations SERIAL PRIMARY KEY,
+    title VARCHAR(255),
+    thumbnail VARCHAR(255),
+    published_at TIMESTAMP
+);
 
-------------------------------------------------------------
--- Table: Users
-------------------------------------------------------------
-CREATE TABLE public.Users(
-	id_user     SERIAL NOT NULL ,
-	firstname   VARCHAR (50) NOT NULL ,
-	pseudo      VARCHAR (50) NOT NULL UNIQUE ,
-	lastname    VARCHAR (50) NOT NULL ,
-	birth       DATE  NOT NULL ,
-	mail        VARCHAR (255) NOT NULL UNIQUE,
-	country     VARCHAR (50) NOT NULL ,
-	phone       VARCHAR (15) NOT NULL ,
-	password    VARCHAR (255) NOT NULL ,
-	picture     VARCHAR (255) NOT NULL  ,
-	CONSTRAINT Users_PK PRIMARY KEY (id_user)
-)WITHOUT OIDS;
+CREATE TABLE SearchCollaborations (
+    id_searchcollaborations SERIAL PRIMARY KEY,
+    title VARCHAR(255),
+    thumbnail VARCHAR(255),
+    published_at TIMESTAMP
+);
 
+CREATE TABLE Learnings (
+    id_learning SERIAL PRIMARY KEY,
+    title VARCHAR(255),
+    thumbnail VARCHAR(255),
+    description TEXT,
+    price DECIMAL,
+    date DATE,
+    rate INT
+);
 
-------------------------------------------------------------
--- Table: Collaboration
-------------------------------------------------------------
-CREATE TABLE public.Collaboration(
-	id_collaboration   SERIAL NOT NULL ,
-	title              VARCHAR (50) NOT NULL ,
-	thumbnail          VARCHAR (255) NOT NULL ,
-	published_at       DATE  NOT NULL  ,
-	CONSTRAINT Collaboration_PK PRIMARY KEY (id_collaboration)
-)WITHOUT OIDS;
+CREATE TABLE Filters (
+    id_filter SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
 
+CREATE TABLE MediaType (
+    id_media_type SERIAL PRIMARY KEY,
+    label VARCHAR(50)
+);
 
-------------------------------------------------------------
--- Table: UsersCollaborations
-------------------------------------------------------------
-CREATE TABLE public.UsersCollaborations(
-	id_usercollaboration   SERIAL NOT NULL ,
-	id_user                INT  NOT NULL ,
-	id_collaboration       INT  NOT NULL  ,
-	CONSTRAINT UsersCollaborations_PK PRIMARY KEY (id_usercollaboration)
+CREATE TABLE Media (
+    id_media SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    id_collaborations INT,
+    FOREIGN KEY (id_collaborations) REFERENCES Collaborations(id_collaborations)
+);
 
-	,CONSTRAINT UsersCollaborations_Users_FK FOREIGN KEY (id_user) REFERENCES public.Users(id_user)
-	,CONSTRAINT UsersCollaborations_Collaboration0_FK FOREIGN KEY (id_collaboration) REFERENCES public.Collaboration(id_collaboration)
-)WITHOUT OIDS;
+CREATE TABLE Chats (
+    id_chat SERIAL PRIMARY KEY,
+    sender INT,
+    receiver INT,
+    message TEXT,
+    created_at TIMESTAMP,
+    FOREIGN KEY (sender) REFERENCES Users(id_user),
+    FOREIGN KEY (receiver) REFERENCES Users(id_user)
+);
 
+CREATE TABLE UserFilters (
+    id_userfilter SERIAL PRIMARY KEY,
+    id_user INT,
+    id_filter INT,
+    FOREIGN KEY (id_user) REFERENCES Users(id_user),
+    FOREIGN KEY (id_filter) REFERENCES Filters(id_filter)
+);
 
-------------------------------------------------------------
--- Table: Learnings
-------------------------------------------------------------
-CREATE TABLE public.Learnings(
-	id_learning   SERIAL NOT NULL ,
-	title         VARCHAR (50) NOT NULL ,
-	thumbnail     VARCHAR (255) NOT NULL ,
-	description   VARCHAR (255) NOT NULL ,
-	price         DECIMAL (10,2)  NOT NULL ,
-	date          DATE  NOT NULL ,
-	rate          INT  NOT NULL  ,
-	CONSTRAINT Learnings_PK PRIMARY KEY (id_learning)
-)WITHOUT OIDS;
+CREATE TABLE SearchCollaborationFilters (
+    id_searchcollaborationfilters SERIAL PRIMARY KEY,
+    id_filter INT,
+    id_searchcollaborations INT,
+    FOREIGN KEY (id_filter) REFERENCES Filters(id_filter),
+    FOREIGN KEY (id_searchcollaborations) REFERENCES SearchCollaborations(id_searchcollaborations)
+);
 
+CREATE TABLE UserCollaborations (
+    id_usercollaborations SERIAL PRIMARY KEY,
+    id_user INT,
+    id_collaborations INT,
+    FOREIGN KEY (id_user) REFERENCES Users(id_user),
+    FOREIGN KEY (id_collaborations) REFERENCES Collaborations(id_collaborations)
+);
 
-------------------------------------------------------------
--- Table: MediaType
-------------------------------------------------------------
-CREATE TABLE public.MediaType(
-	id_media_type   SERIAL NOT NULL ,
-	label           VARCHAR (50) NOT NULL  ,
-	CONSTRAINT MediaType_PK PRIMARY KEY (id_media_type)
-)WITHOUT OIDS;
+CREATE TABLE UserLearnings (
+    id_userlearning SERIAL PRIMARY KEY,
+    id_user INT,
+    id_learning INT,
+    FOREIGN KEY (id_user) REFERENCES Users(id_user),
+    FOREIGN KEY (id_learning) REFERENCES Learnings(id_learning)
+);
 
+CREATE TABLE LearningFilters (
+    id_learningfilters SERIAL PRIMARY KEY,
+    id_learning INT,
+    id_filter INT,
+    FOREIGN KEY (id_learning) REFERENCES Learnings(id_learning),
+    FOREIGN KEY (id_filter) REFERENCES Filters(id_filter)
+);
 
-------------------------------------------------------------
--- Table: Media
-------------------------------------------------------------
-CREATE TABLE public.Media(
-	id_media           SERIAL NOT NULL ,
-	name               VARCHAR (50) NOT NULL ,
-	id_media_type      INT  NOT NULL ,
-	id_collaboration   INT   ,
-	id_learning        INT    ,
-	CONSTRAINT Media_PK PRIMARY KEY (id_media)
-
-	,CONSTRAINT Media_MediaType_FK FOREIGN KEY (id_media_type) REFERENCES public.MediaType(id_media_type)
-	,CONSTRAINT Media_Collaboration0_FK FOREIGN KEY (id_collaboration) REFERENCES public.Collaboration(id_collaboration)
-	,CONSTRAINT Media_Learnings1_FK FOREIGN KEY (id_learning) REFERENCES public.Learnings(id_learning)
-)WITHOUT OIDS;
-
-
-------------------------------------------------------------
--- Table: UsersLearnings
-------------------------------------------------------------
-CREATE TABLE public.UsersLearnings(
-	id_userlearning   SERIAL NOT NULL ,
-	id_user           INT  NOT NULL ,
-	id_learning       INT  NOT NULL  ,
-	CONSTRAINT UsersLearnings_PK PRIMARY KEY (id_userlearning)
-
-	,CONSTRAINT UsersLearnings_Users_FK FOREIGN KEY (id_user) REFERENCES public.Users(id_user)
-	,CONSTRAINT UsersLearnings_Learnings0_FK FOREIGN KEY (id_learning) REFERENCES public.Learnings(id_learning)
-)WITHOUT OIDS;
-
-
-------------------------------------------------------------
--- Table: Filters
-------------------------------------------------------------
-CREATE TABLE public.Filters(
-	id_filter   SERIAL NOT NULL ,
-	name        VARCHAR (50) NOT NULL  ,
-	CONSTRAINT Filters_PK PRIMARY KEY (id_filter)
-)WITHOUT OIDS;
-
-
-------------------------------------------------------------
--- Table: UsersFilters
-------------------------------------------------------------
-CREATE TABLE public.UsersFilters(
-	id_userfilter   SERIAL NOT NULL ,
-	id_filter       INT  NOT NULL ,
-	id_user         INT  NOT NULL  ,
-	CONSTRAINT UsersFilters_PK PRIMARY KEY (id_userfilter)
-
-	,CONSTRAINT UsersFilters_Filters_FK FOREIGN KEY (id_filter) REFERENCES public.Filters(id_filter)
-	,CONSTRAINT UsersFilters_Users0_FK FOREIGN KEY (id_user) REFERENCES public.Users(id_user)
-)WITHOUT OIDS;
-
-------------------------------------------------------------
--- Table: Chats
-------------------------------------------------------------
-CREATE TABLE public.Chats(
-	id_chat      SERIAL NOT NULL ,
-	sender       VARCHAR (50) NOT NULL ,
-	receiver     VARCHAR (50) NOT NULL ,
-	message      VARCHAR (2000)  NOT NULL ,
-	created_at   TIMESTAMP  NOT NULL  ,
-	CONSTRAINT Chats_PK PRIMARY KEY (id_chat)
-)WITHOUT OIDS;
-
-
-------------------------------------------------------------
--- Table: UsersChats
-------------------------------------------------------------
-CREATE TABLE public.UsersChats(
-	id_userchat   SERIAL NOT NULL ,
-	id_user       INT  NOT NULL ,
-	id_chat       INT  NOT NULL  ,
-	CONSTRAINT UsersChats_PK PRIMARY KEY (id_userchat)
-
-	,CONSTRAINT UsersChats_Users_FK FOREIGN KEY (id_user) REFERENCES public.Users(id_user)
-	,CONSTRAINT UsersChats_Chats0_FK FOREIGN KEY (id_chat) REFERENCES public.Chats(id_chat)
-)WITHOUT OIDS;
-
-------------------------------------------------------------
+ALTER SEQUENCE users_id_user_seq RESTART WITH 1;
+ALTER SEQUENCE collaborations_id_collaborations_seq RESTART WITH 1;
+ALTER SEQUENCE searchcollaborations_id_searchcollaborations_seq RESTART WITH 1;
+ALTER SEQUENCE learnings_id_learning_seq RESTART WITH 1;
+ALTER SEQUENCE filters_id_filter_seq RESTART WITH 1;
+ALTER SEQUENCE mediatype_id_media_type_seq RESTART WITH 1;
+ALTER SEQUENCE media_id_media_seq RESTART WITH 1;
+ALTER SEQUENCE chats_id_chat_seq RESTART WITH 1;
+ALTER SEQUENCE userfilters_id_userfilter_seq RESTART WITH 1;
+ALTER SEQUENCE searchcollaborationfilters_id_searchcollaborationfilters_seq RESTART WITH 1;
+ALTER SEQUENCE usercollaborations_id_usercollaborations_seq RESTART WITH 1;
+ALTER SEQUENCE userlearnings_id_userlearning_seq RESTART WITH 1;
+ALTER SEQUENCE learningfilters_id_learningfilters_seq RESTART WITH 1;
 
 -- Insertion des données dans la table MediaType
 INSERT INTO public.MediaType (label) VALUES
@@ -161,18 +136,18 @@ INSERT INTO public.MediaType (label) VALUES
 ('Image'),
 ('Music');
 
--- Insertion des données dans la table Collaboration
-INSERT INTO public.Collaboration (title, thumbnail, published_at) VALUES
-('ProjectA', '/assets/media/collaboration/thumbnail/ProjectA.jpg', '2023-01-15'),
-('ProjectB', '/assets/media/collaboration/thumbnail/ProjectB.jpg', '2023-02-20'),
-('ProjectC', '/assets/media/collaboration/thumbnail/ProjectC.jpg', '2023-03-25'),
-('ProjectD', '/assets/media/collaboration/thumbnail/ProjectD.jpg', '2023-04-30'),
-('ProjectE', '/assets/media/collaboration/thumbnail/ProjectE.jpg', '2023-05-10'),
-('ProjectF', '/assets/media/collaboration/thumbnail/ProjectF.jpg', '2023-06-15'),
-('ProjectG', '/assets/media/collaboration/thumbnail/ProjectG.jpg', '2023-07-20'),
-('ProjectH', '/assets/media/collaboration/thumbnail/ProjectH.jpg', '2023-08-25'),
-('ProjectI', '/assets/media/collaboration/thumbnail/ProjectI.jpg', '2023-09-30'),
-('ProjectJ', '/assets/media/collaboration/thumbnail/ProjectJ.jpg', '2023-10-10');
+-- Insertion des données dans la table Users
+INSERT INTO public.Users (firstname, pseudo, lastname, birth, mail, country, phone, password, picture) VALUES
+('Alice', 'alice01', 'Dupont', '1990-01-01', 'alice@example.com', 'France', '0123456789', 'password123', '/assets/media/user/picture/alice01.jpg'),
+('Bob', 'bobster', 'Martin', '1985-02-14', 'bob@example.com', 'France', '0987654321', 'password123', '/assets/media/user/picture/bobster.jpg'),
+('Charlie', 'charlie2020', 'Durand', '1992-03-03', 'charlie@example.com', 'France', '0123987456', 'password123', '/assets/media/user/picture/charlie2020.jpg'),
+('David', 'dave', 'Lefevre', '1988-04-22', 'david@example.com', 'France', '0543219876', 'password123', '/assets/media/user/picture/dave.jpg'),
+('Eve', 'eve123', 'Petit', '1995-05-30', 'eve@example.com', 'France', '0678901234', 'password123', '/assets/media/user/picture/eve123.jpg'),
+('Frank', 'frankie', 'Moreau', '1987-06-18', 'frank@example.com', 'France', '0789012345', 'password123', '/assets/media/user/picture/frankie.jpg'),
+('Grace', 'graceful', 'Roux', '1991-07-07', 'grace@example.com', 'France', '0890123456', 'password123', '/assets/media/user/picture/graceful.jpg'),
+('Hank', 'hanky', 'Simon', '1986-08-19', 'hank@example.com', 'France', '0901234567', 'password123', '/assets/media/user/picture/hanky.jpg'),
+('Ivy', 'ivyivy', 'Blanc', '1993-09-23', 'ivy@example.com', 'France', '0012345678', 'password123', '/assets/media/user/picture/ivyivy.jpg'),
+('Jack', 'jacko', 'Faure', '1989-10-10', 'jack@example.com', 'France', '0912345678', 'password123', '/assets/media/user/picture/jacko.jpg');
 
 -- Insertion des données dans la table Learnings
 INSERT INTO public.Learnings (title, thumbnail, description, price, date, rate) VALUES
@@ -186,6 +161,46 @@ INSERT INTO public.Learnings (title, thumbnail, description, price, date, rate) 
 ('LearningH', '/assets/media/learning/thumbnail/LearningH.jpg', 'Description H', 79.99, '2023-08-20', 4),
 ('LearningI', '/assets/media/learning/thumbnail/LearningI.jpg', 'Description I', 89.99, '2023-09-25', 4),
 ('LearningJ', '/assets/media/learning/thumbnail/LearningJ.jpg', 'Description J', 99.99, '2023-10-30', 5);
+
+-- Insertion des données dans la table Collaboration
+INSERT INTO public.Collaborations (title, thumbnail, published_at) VALUES
+('ProjectA', '/assets/media/collaboration/thumbnail/ProjectA.jpg', '2023-01-15'),
+('ProjectB', '/assets/media/collaboration/thumbnail/ProjectB.jpg', '2023-02-20'),
+('ProjectC', '/assets/media/collaboration/thumbnail/ProjectC.jpg', '2023-03-25'),
+('ProjectD', '/assets/media/collaboration/thumbnail/ProjectD.jpg', '2023-04-30'),
+('ProjectE', '/assets/media/collaboration/thumbnail/ProjectE.jpg', '2023-05-10'),
+('ProjectF', '/assets/media/collaboration/thumbnail/ProjectF.jpg', '2023-06-15'),
+('ProjectG', '/assets/media/collaboration/thumbnail/ProjectG.jpg', '2023-07-20'),
+('ProjectH', '/assets/media/collaboration/thumbnail/ProjectH.jpg', '2023-08-25'),
+('ProjectI', '/assets/media/collaboration/thumbnail/ProjectI.jpg', '2023-09-30'),
+('ProjectJ', '/assets/media/collaboration/thumbnail/ProjectJ.jpg', '2023-10-10');
+
+-- Insertion des données dans la table UsersCollaborations
+INSERT INTO public.UserCollaborations (id_user, id_collaborations) VALUES
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 4),
+(5, 5),
+(6, 6),
+(7, 7),
+(8, 8),
+(9, 9),
+(10, 10);
+
+
+-- Insertion des données dans la table UsersLearnings
+INSERT INTO public.UserLearnings (id_user, id_learning) VALUES
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 4),
+(5, 5),
+(6, 6),
+(7, 7),
+(8, 8),
+(9, 9),
+(10, 10);
 
 -- Insertion des données dans la table Filters
 INSERT INTO public.Filters (name) VALUES 
@@ -342,48 +357,3 @@ INSERT INTO public.Filters (name) VALUES
 ('Funk Rap'),
 ('New wave'),
 ('Electro Trap');
-
--- Insertion des données dans la table UsersCollaborations
-INSERT INTO public.UsersCollaborations (id_user, id_collaboration) VALUES
-(1, 1),
-(2, 2),
-(3, 3),
-(4, 4),
-(5, 5),
-(6, 6),
-(7, 7),
-(8, 8),
-(9, 9),
-(10, 10);
-
-
--- Insertion des données dans la table UsersLearnings
-INSERT INTO public.UsersLearnings (id_user, id_learning) VALUES
-(1, 1),
-(2, 2),
-(3, 3),
-(4, 4),
-(5, 5),
-(6, 6),
-(7, 7),
-(8, 8),
-(9, 9),
-(10, 10);
-
--- Insertion des données dans la table Media
-INSERT INTO public.Media (name, id_media_type, id_collaboration, id_learning) VALUES
-('CollaborationDocument1', 1, 1, NULL),
-('CollaborationImage1', 2, 2, NULL),
-('LearningVideo3', 1, NULL, NULL),
-('CollaborationPodcast1', 3, 3, NULL),
-('CollaborationInfographic1', 2, 4, NULL),
-('LearningA', 1, NULL, 1),
-('LearningB', 1, NULL, 2),
-('LearningC', 3, NULL, 3),
-('LearningD', 1, NULL, 4),
-('LearningE', 1, NULL, 5),
-('LearningF', 1, NULL, 6),
-('LearningG', 1, NULL, 7),
-('LearningH', 1, NULL, 8),
-('LearningI', 3, NULL, 9),
-('LearningJ', 1, NULL, 10);
