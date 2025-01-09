e766c71a5da806e652d435ebeefdff0fa7f79e80
Arthur <arthur.garnier1090@gmail.com>
2025-01-09 10:42:38 +0100

    update readme + gitignore and config.ini

--- models/database.php
+++ models/database.php
@@ -1,17 +1,24 @@
 <?php
+
+require_once 'config.ini';
 class Database
 {
-    private $host = "localhost";
-    private $port = "5432";
-    private $db_name = "collabdoor";
-    private $username = "postgres";
-<<<<<<< HEAD
-    private $password = "Mapaaskla1";
-=======
-    private $password = "Dorian93120";
->>>>>>> 53758ff05ee9f7e7f3a0b46e2162997876fb5b43
+    private $host;
+    private $port;
+    private $db_name;
+    private $username;
+    private $password;
     private $conn;
 
+    public function __construct()
+    {
+        $config = parse_ini_file('../config.ini', true);
+        $this->host = $config['database']['host'];
+        $this->port = $config['database']['port'];
+        $this->db_name = $config['database']['dbname'];
+        $this->username = $config['database']['username'];
+        $this->password = $config['database']['password'];
+    }
     public function connect()
     {
         if ($this->conn === null) {
